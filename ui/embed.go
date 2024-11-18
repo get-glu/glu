48fe222445b1bbe0421c35c854aada0fc07470a1
George <me@georgemac.com>
2024-11-18 18:07:38 +0000

    fix: expose UI embed as function (#38)

--- ui/embed.go
+++ ui/embed.go
@@ -2,7 +2,18 @@ package ui
 
 import (
 	"embed"
+	"io/fs"
 )
 
-//go:embed dist
-var Assets embed.FS
+//go:embed dist/*
+var ui embed.FS
+
+// FS returns the embedded distribution of the Glu UI.
+func FS() fs.FS {
+	fs, err := fs.Sub(ui, "dist")
+	if err != nil {
+		panic(err)
+	}
+
+	return fs
+}
