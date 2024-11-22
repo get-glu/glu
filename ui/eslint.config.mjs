0e36c4f5e6dce1a9cabb487f4d5a51834046359e
Mark Phelps <209477+markphelps@users.noreply.github.com>
2024-11-22 13:35:25 -0500

    chore: fix eslint config
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- ui/eslint.config.mjs
+++ ui/eslint.config.mjs
@@ -2,13 +2,25 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname
+});
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
+  { settings: { 'react': { version: 'detect' } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  { extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'] }
+  ...compat.extends('plugin:prettier/recommended'),
 ];
