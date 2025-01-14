86fede65d43b0fd5d2db7d9a21af4cd8dad07056
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-01-14 12:28:09 -0500

    chore: make source a first class thing
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -26,7 +26,6 @@ type PipelineConfig struct {
 type SourceConfig struct {
 	Kind   string                 `yaml:"kind"`
 	Name   string                 `yaml:"name"`
-	SCM    string                 `yaml:"scm,omitempty"`
 	Config map[string]interface{} `yaml:"config,omitempty"`
 }
 
@@ -70,8 +69,13 @@ func parsePipeline(_ context.Context, cfg PipelineConfig) (*core.Pipeline, error
 
 		// Create phase descriptor
 		desc := core.Descriptor{
-			Kind:     sourceConfig.Kind,
+			Pipeline: cfg.Name,
 			Metadata: phaseMeta,
+			Source: core.Source{
+				Kind:   sourceConfig.Kind,
+				Name:   sourceConfig.Name,
+				Config: sourceConfig.Config,
+			},
 		}
 
 		// Create the phase
